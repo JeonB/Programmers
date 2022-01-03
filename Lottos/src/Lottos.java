b82a15f24664c1755197cde5485088bf9c32ee93
JeonB <coconutpig26@gmail.com>
2022-01-03 15:48:05 +0900

    카카오 추천id 생성하기
    추천아이디만들기 알고리즘

--- Lottos/src/Lottos.java
+++ Lottos/src/Lottos.java
@@ -1,7 +1,4 @@
 
-import java.util.Arrays;
-import java.util.stream.IntStream;
-
 public class Lottos {
 
     public static void main(String[] args) {
@@ -12,26 +9,35 @@ public class Lottos {
         solution(lottos, win_nums);
     }
 
-    @SuppressWarnings("unlikely-arg-type")
     public static int[] solution(int[] lottos, int[] win_nums) {
-        int count1 = 7; // 등수
-        int count2 = 0; // 0의 개수 구하기
-
-        int min = 0, max = 0;
+        int count1 = 7; // 등수 결정 변수
+        int count2 = 0; // 0의 개수
+        int max = 0;
         for (int i = 0; i < win_nums.length; i++) {
-            int a = win_nums[i];
-            if (Arrays.asList(win_nums).contains(a)) { // 디버거를 돌려 확인해보니 false가 발생
-                count1--;
+            for (int j = 0; j < win_nums.length; j++) {
+                if (lottos[j] == win_nums[i]) {
+                    count1--;
+                }
+                // if (Arrays.asList(win_nums).contains(a)) { // 디버거를 돌려 확인해보니 false가 발생
+                // count1--;
+                // }
+            }
+
+        }
+        for (int j = 0; j < lottos.length; j++) {
+            if (lottos[j] == 0) {
+                count2++;
             }
         }
-        if (count1 == 7) {
+
+        if (count1 == 7 || count1 == 6) {
             count1 = 6;
         }
-        System.out.println("당첨 등수는 " + count1 + "입니다!");
-        min = count1;
 
-        int[] answer = { max, min };
+        max = count1 - count2;
+        System.out.println("당첨 등수는 " + max + "입니다!");
+
+        int[] answer = { max, count1 };
         return answer;
     }
-
 }
---
+++ RecommendId/src/RecommendId.java
@@ -0,0 +1,14 @@
+public class RecommendId {
+    public static void main(String[] args) throws Exception {
+
+    }
+
+    public String solution(String new_id) {
+        String[][] stage = new String[7][100];
+        stage[0][0] = new_id.toLowerCase();
+
+        String answer = "";
+        return answer;
+    }
+
+}
