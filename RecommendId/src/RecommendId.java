789648a656b539be132385ce65149b6bfa83b159
JeonB <coconutpig26@gmail.com>
2022-03-08 20:02:19 +0900

    사소한 수정

--- RecommendId/src/RecommendId.java
+++ RecommendId/src/RecommendId.java
@@ -1,6 +1,6 @@
 public class RecommendId {
     public static void main(String[] args) throws Exception {
-        System.out.println(solution("abcdefghijklmn.p"));
+        System.out.println(solution("Aabcdefghijklmn.p"));
     }
 
     public static String solution(String new_id) {
@@ -8,7 +8,7 @@ public class RecommendId {
         char lastChar;
 
         new_id = new_id.toLowerCase(); // 1단계 대문자를 소문자로 변경
-        new_id = new_id.replaceAll("[^a-z0-9-_.]", ""); // 2단계 소문자,숫자,-,_,. 제외한 모든 문자 제거
+        new_id = new_id.replaceAll("[^\\w\\d-_.]", ""); // 2단계 소문자,숫자,-,_,. 제외한 모든 문자 제거
         new_id = new_id.replaceAll("[.]{2,}", "."); // 3단계 마침표가 2개이상일 때 한 개로 치환
         new_id = new_id.replaceAll("^[.]|[.]$", ""); // 4단계 마침표(.)가 처음이나 끝에 위치한다면 제거
 
