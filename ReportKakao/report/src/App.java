bf13e9fa750894bc2d53e61b5187f53f6d82db6e
JeonB <newjeonb@gmail.com>
2022-05-10 12:15:15 +0900

    testing

--- ReportKakao/report/src/App.java
+++ ReportKakao/report/src/App.java
@@ -2,9 +2,7 @@ import java.util.*;
 
 public class App {
 
-  public static void main(String[] args) throws Exception {
-    System.out.println("Hello, World!");
-  }
+  public static void main(String[] args) throws Exception {}
 
   class Solution {
 
