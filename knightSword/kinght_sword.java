9c80b0a412d284ef3361e074abf955b4714e20cd
JeonB <newjeonb@gmail.com>
2022-11-19 22:23:14 +0900

    기사단원의 무기

---
+++ knightSword/kinght_sword.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        int [] exponent = new [number];
+        // 2,3,5,7 소인수분해 프로그램 만들어보자
+        for(int i = 1 ; i < number+1 ; i++){
+            i%2
+        }
+        return answer;
+    }
+}
+
+
+if(i%2 == 0){
+    
+}
