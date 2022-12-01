2e5c81b13669854e596f97cca315abe7d4703b7a
JeonB <newjeonb@gmail.com>
2022-12-01 23:33:56 +0900

    아 졸령

---
+++ Split/splitstr.java
@@ -0,0 +1,14 @@
+public class splitstr {
+    class Solution {
+        public int solution(String s) {
+            int answer = 0;
+            return answer;
+        }
+    }
+    if()
+    string firstChar = s[0]
+    string otherChar
+    1) 검사하는 첫 문자의 개수 == 첫 문자가 아닌 문자의 개수 -> 제외 result값 증가
+    2) 반복 + result값 증가
+    3) 문자열 끝에 다다르면 종료 및 result값 반환
+}
