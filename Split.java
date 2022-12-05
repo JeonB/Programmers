216774cdd2e4a94fc22c3723b7819f3cfd613247
JeonB <newjeonb@gmail.com>
2022-12-05 16:33:37 +0900

    split

---
+++ Split.java
@@ -0,0 +1,21 @@
+class Split {
+
+  public static void main(String[] args) {
+    System.out.println("test");
+  }
+
+  public int solution(String s) {
+    int answer = 0;
+    System.out.println("dfd");
+    return answer;
+  }
+}
+//     ex str = "aaabbbcc";
+//     -> return 2
+//     if()
+//     string firstChar = s[0]
+//     string otherChar
+//     1) 검사하는 첫 문자의 개수 == 첫 문자가 아닌 문자의 개수 -> 제외 result값 증가
+//     2) 반복 + result값 증가
+//     3) 문자열 끝에 다다르면 종료 및 result값 반환
+// }
