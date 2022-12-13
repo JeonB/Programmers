f312416d53f2d111f11323c309222ffd67600722
JeonB <newjeonb@gmail.com>
2022-12-13 13:39:00 +0900

    햄버거 만들기

---
+++ MakeHamburger/MakeHamburger.java
@@ -0,0 +1,25 @@
+package MakeHamburger;
+
+public class MakeHamburger {
+    
+}
+
+
+
+class Solution {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        return answer;
+    }
+}
+
+//if(ingrediant[i]<ingrediant[length - 4]){
+if(ingredinat[i]==1) ingrediant[i+1] ==2ingrediant[i+2]==3 ingrediant[i+3] == 1)
+    arr to list 
+    delete list 
+    list to arr
+    answer++
+    insert arr
+    if(ingredinat[i+])
+    
+}
