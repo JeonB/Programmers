c1f551dfdf66f470a0df2eb85c38972a43efba93
JeonB <newjeonb@gmail.com>
2022-12-14 20:08:48 +0900

    test

--- MakeHamburger/MakeHamburger.java
+++ MakeHamburger/MakeHamburger.java
@@ -7,21 +7,7 @@ public class MakeHamburger {
   public static void main(String[] args) {}
 
   public int solution(int[] ingredient) {
-    int answer = 0;
+    int answer = 0;dddd
     return answer;
   }
 }
-/* class Solution {
- test
-}
-
-if(i<length - 3){
-if(ingredinat[i]==1 ingrediant[i+1] ==2 ingrediant[i+2]==3 ingrediant[i+3] == 1)
-    arr to list 
-    delete list 
-    list to arr
-    answer++
-    insert arr
-    if(ingredinat[i+])
-    
-} */
