bc3e2b513c7f204842b7da16300a06c0c0fae045
JeonB <newjeonb@gmail.com>
2022-12-19 16:59:28 +0900

    옹알이

--- Babbling/Babbling.java
+++ Babbling/Babbling.java
@@ -1,13 +1,31 @@
 package Babbling;
 
 public class Babbling {
-    
-}
-
-
-class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
         return answer;
-    }
+    }    
 }
+
+
+/* 
+"aya", "ye", "woo", "ma" 
+
+if(babling[i].charAt[0]+babling[i].charAt[1] == "ye" or "ma")
+else(babling[i].charAt[0]+babling[i].charAt[1] == "aya" or "woo")
+
+
+String babling = "ayaye"
+
+if(babling[i].getChars(j,j+2) == "ye" or "ma")
+    j+=2
+    solution
+else if (babling[i].getChars(j,j+3) == "aya" or "woo")
+    j+=3
+    solution
+else continue;
+
+
+
+
+*/
