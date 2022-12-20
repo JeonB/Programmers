cd58ad942b33393be66213b5a15d8fb51ea2a550
JeonB <newjeonb@gmail.com>
2022-12-20 20:07:44 +0900

    숫자 짝꿍(혼자 못 풂)

--- NumberPartner/NumberPartner.java
+++ NumberPartner/NumberPartner.java
@@ -1,17 +1,39 @@
 package NumberPartner;
 
-import java.util.stream.IntStream;
 import java.util.*;
 
-public class NumberPartner {
-
-
-    public String solution(String X, String Y) {
+class Solution {
+    public static void main(String[] args) {
+        System.out.println(solution("3403","13203"));
+    }
+    public static String solution(String X, String Y) {
+        
+        int[] cntX = new int[10];
+        int[] cntY = new int[10];
+        
+        for (String tmpX : X.split("")) cntX[Integer.parseInt(tmpX)]++;
+        for (String tmpY : Y.split("")) cntY[Integer.parseInt(tmpY)]++;
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 9; i >= 0; i--){
+            while(cntX[i] > 0 && cntY[i] > 0){
+                sb.append(i);
+                cntX[i]--;
+                cntY[i]--;
+            }
+        }
+        
         String answer = "";
-        return answer;
+        
+        if ("".equals(sb.toString())) {
+            answer = "-1";
+        } else if ("0".equals(sb.toString().substring(0,1))) {
+            answer = "0";
+        } else {
+            answer = sb.toString();
+        }
 
-        List<String> list = new ArrayList<String>(Arrays.asList(X));
-      
+        return answer;
     }
 }
-
