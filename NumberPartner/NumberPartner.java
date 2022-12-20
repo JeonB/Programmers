399500ba3d6e74b0b9d8fda34f76fb68e73660df
JeonB <newjeonb@gmail.com>
2022-12-20 16:57:56 +0900

    흠

---
+++ NumberPartner/NumberPartner.java
@@ -0,0 +1,17 @@
+package NumberPartner;
+
+import java.util.stream.IntStream;
+import java.util.*;
+
+public class NumberPartner {
+
+
+    public String solution(String X, String Y) {
+        String answer = "";
+        return answer;
+
+        List<String> list = new ArrayList<String>(Arrays.asList(X));
+      
+    }
+}
+
