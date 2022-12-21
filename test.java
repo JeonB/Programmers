84be48205d320bdde1eca85d49e669dbaf3982fa
JeonB <newjeonb@gmail.com>
2022-12-21 16:25:34 +0900

    푸트파이트대회

---
+++ test.java
@@ -0,0 +1,11 @@
+public class test {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+    
+    sb.insert(6, "Jon");
+
+    System.out.println(new String(sb));
+    }
+
+
+}
