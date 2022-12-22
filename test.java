7317b1a70ad3f308d39e400bb7023eb58f4468f4
JeonB <newjeonb@gmail.com>
2022-12-22 12:17:23 +0900

    hm

--- test.java
+++ test.java
@@ -1,12 +1,15 @@
 public class test {
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder();
-        String test = new String();
+        StringBuilder sb = new StringBuilder(3);
+        String test = new String("hi"); //추천하지 않는 방법
         String test2 = "sibbalk";
-
+        int i = 1;
+        String a = i + test2 + i;
+        System.out.println(a);
         sb.append(test2);
         sb.insert(6, "Jon");
-        System.out.println(new String(sb));
+        sb.toString();
+        System.out.println(sb);
     }
 
 
