6c6993cb0acd9fb26bb51df352d4235cf9d06b15
JeonB <newjeonb@gmail.com>
2022-12-23 16:38:53 +0900

    수포자

---
+++ 수포자/mathLoser.java
@@ -0,0 +1,10 @@
+package 수포자;
+public static void main(String[] args) {
+    
+}
+class Solution {
+    public int[] solution(int[] answers) {
+        int[] answer = {};
+        return answer;
+    }
+}
