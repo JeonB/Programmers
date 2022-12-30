876858adbef40353788586f5e637e4e908e4414b
JeonB <newjeonb@gmail.com>
2022-12-30 14:43:34 +0900

    소수만들기

---
+++ MakePrimeNum/makePrimeNum.java
@@ -0,0 +1,27 @@
+package MakePrimeNum;
+
+class Solution {
+    public static void main(String[] args) {
+       int[] nums = {1,2,7,6,4};
+       System.out.println(solution(nums));
+    }
+    public static int solution(int[] nums) {
+        int answer = -1;
+        int sum = 0;
+        for(int i = 0 ; i<nums.length-3;i++){
+            for(int j = i+1 ; j<nums.length -2 ; j++){
+                for(int k = j+1; k<nums.length - 1 ; k++){
+                    sum = nums[i] + nums[j] + nums[k];
+                }
+            }
+        }
+
+        return answer;
+    }
+    public static boolean isPrime(int num){
+        boolean prime;
+        
+
+        return 
+    }
+}
