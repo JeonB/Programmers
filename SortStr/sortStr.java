2712588babba7c14ad733cb0c0f0ccc7e3361ebf
JeonB <newjeonb@gmail.com>
2023-02-25 17:48:20 +0900

    문자열 내 마음대로 자르고 정렬하기

--- SortStr/sortStr.java
+++ SortStr/sortStr.java
@@ -23,7 +23,7 @@ public class sortStr {
     }
     return answer;
   }
-  /*  기준 문자를 꺼내서 기존 문자열에 붙인다는 발상이 놀랍다. arr.add()에서 ""의 의미가 뭘까?
+  /*  기준 문자를 꺼내서 기존 문자열에 붙인다는 발상이 놀랍다. arr.add()에서 ""의 의미가 뭘까?. 하지만, 실행속도가 너무 느리다는 단점이 있다.
    String[] answer = {};
         ArrayList<String> arr = new ArrayList<>();
         for (int i = 0; i < strings.length; i++) {
@@ -36,4 +36,17 @@ public class sortStr {
         }
         return answer;
    */
+
+  /* 실행속도가 훨씬 빠르고 자바적으로 풀은 답이다.
+    Arrays.sort(strings, new Comparator<String>(){
+          @Override
+          public int compare(String s1, String s2){
+              if(s1.charAt(n) > s2.charAt(n)) return 1;
+              else if(s1.charAt(n) == s2.charAt(n)) return s1.compareTo(s2);
+              else if(s1.charAt(n) < s2.charAt(n)) return -1;
+              else return 0;
+          }
+      });
+      return strings;
+    */
 }
