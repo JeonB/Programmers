54f85d3feef615c0d0db83c57017198f1b8827db
JeonB <newjeonb@gmail.com>
2023-02-25 17:42:40 +0900

    문자열 내마음대로 자르기

--- SortStr/sortStr.java
+++ SortStr/sortStr.java
@@ -23,4 +23,17 @@ public class sortStr {
     }
     return answer;
   }
+  /*  기준 문자를 꺼내서 기존 문자열에 붙인다는 발상이 놀랍다. arr.add()에서 ""의 의미가 뭘까?
+   String[] answer = {};
+        ArrayList<String> arr = new ArrayList<>();
+        for (int i = 0; i < strings.length; i++) {
+            arr.add("" + strings[i].charAt(n) + strings[i]);
+        }
+        Collections.sort(arr);
+        answer = new String[arr.size()];
+        for (int i = 0; i < arr.size(); i++) {
+            answer[i] = arr.get(i).substring(1, arr.get(i).length());
+        }
+        return answer;
+   */
 }
