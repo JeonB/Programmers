c68f013745ed65a6234d486672481c8994d8d8c0
JeonB <newjeonb@gmail.com>
2023-03-09 18:18:54 +0900

    제일작은수제거

---
+++ 제일작은수제거/MinNumDelete.java
@@ -0,0 +1,27 @@
+package 제일작은수제거;
+
+import java.util.Arrays;
+
+public class MinNumDelete {
+
+  public int[] solution(int[] arr) {
+    int[] answer = new int[arr.length - 1];
+    if (arr.length == 1) {
+      arr[0] = -1;
+      return arr;
+    }
+
+    int min = arr[0];
+    for (int i = 1; i < arr.length; i++) {
+      if (min > arr[i]) min = arr[i];
+    }
+    int j = 0;
+    for (int i = 0; i < arr.length; i++) {
+      if (min == arr[i]) continue;
+      answer[j] = arr[i];
+      j++;
+    }
+
+    return answer;
+  }
+}
