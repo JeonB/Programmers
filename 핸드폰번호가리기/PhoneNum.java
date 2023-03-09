fdd50bf85063f692d98895f4f1a504ca5cc1a056
JeonB <newjeonb@gmail.com>
2023-03-09 17:44:27 +0900

    핸드폰번호가리기

---
+++ 핸드폰번호가리기/PhoneNum.java
@@ -0,0 +1,29 @@
+package 핸드폰번호가리기;
+
+public class PhoneNum {
+
+  public static String solution(String phone_number) {
+    String answer = "";
+    if (phone_number.length() > 4) {
+      String temp = phone_number.substring(0, phone_number.length() - 4);
+      answer =
+        temp.replaceAll("\\w", "*") +
+        phone_number.substring(
+          phone_number.length() - 4,
+          phone_number.length()
+        );
+    } else {
+      answer = phone_number.substring(0, 4);
+    }
+    return answer;
+  }
+  /*  모범답안. 4자리일 경우 반복문 패스. 처리 속도가 훨씬 빠르다.
+       public String solution(String phone_number) {
+         char[] ch = phone_number.toCharArray();
+         for(int i = 0; i < ch.length - 4; i ++){
+             ch[i] = '*';
+         }
+         return String.valueOf(ch);
+      }
+       */
+}
