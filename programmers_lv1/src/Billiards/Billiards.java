98cc7a3b91663b2d867f4e9b5fbd868dd450f55a
JeonB <newjeonb@gmail.com>
2023-03-18 22:15:22 +0900

    당구연습

---
+++ programmers_lv1/src/Billiards/Billiards.java
@@ -0,0 +1,11 @@
+package Billiards;
+
+public class Billiards {
+    public int[] solution(int m, int n, int startX, int startY, int[][] balls) {
+        int[] answer = {};
+        return answer;
+    }
+}
+//원쿠션 연습
+// balls는 도착해야 하는 좌표의 위치 배열
+// 입사각과 반사각이 같다
--- programmers_lv1/test/DartGame/DartGameTest.java
+++ programmers_lv1/test/DartGame/DartGameTest.java
@@ -1,4 +1,14 @@
+package DartGame;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class DartGameTest {
-  
+
+    @Test
+    public void solutionTest() {
+        DartGame dg = new DartGame();
+        assertEquals(dg.solution("1D2S3T*"),59);
+    }
 }
