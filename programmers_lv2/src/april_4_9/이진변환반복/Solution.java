6094b9963dde8becae341c066a4b447d45a65d33
JeonB <newjeonb@gmail.com>
2023-04-07 15:26:05 +0900

    이진변환반복 리팩토링

--- programmers_lv2/src/april_4_9/이진변환반복/Solution.java
+++ programmers_lv2/src/april_4_9/이진변환반복/Solution.java
@@ -1,19 +1,20 @@
 package april_4_9.이진변환반복;
 
 class Solution {
+    // 메모리 할당을 최대한 줄이면 줄일수록 당연히 처리 속도는 빨라진다.
     public int[] solution(String s) {
         int binaryOp = 0;
         int delZero = 0;
+        int slength = 0;
         while(s.length() != 1){
-            StringBuilder sb = new StringBuilder();
-
             for(int i= 0 ; i<s.length() ; i++){
                 if(s.charAt(i) == '1' )
-                    sb.append('1');
+                    slength++;
             }
-            delZero += s.length() - sb.length();
+            delZero += s.length() - slength;
             binaryOp++;
-            s = Integer.toBinaryString(sb.length());
+            s = Integer.toBinaryString(slength);
+            slength = 0;
         }
 
         int[] answer = {binaryOp,delZero};
