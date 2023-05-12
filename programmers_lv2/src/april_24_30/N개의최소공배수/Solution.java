0c02d927bcfbb9fa253343e510104c71f3bdadc4
JeonB <newjeonb@gmail.com>
2023-05-12 17:38:29 +0900

    N개의최소공배수

--- programmers_lv2/src/april_24_30/N개의최소공배수/Solution.java
+++ programmers_lv2/src/april_24_30/N개의최소공배수/Solution.java
@@ -1,7 +1,7 @@
 package april_24_30.N개의최소공배수;
 
 import java.util.Arrays;
-
+// n개의 숫자를 담은 배열 arr이 입력되었을 때 이 수들의 최소공배수를 반환하는 함수, solution을 완성
 class Solution {
     public int solution(int[] arr) {
 
