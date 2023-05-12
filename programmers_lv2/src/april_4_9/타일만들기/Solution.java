1bf1b6eabec16184d72437056f0571fb99807163
JeonB <newjeonb@gmail.com>
2023-05-12 18:35:02 +0900

    타일만들기

--- programmers_lv2/src/april_4_9/타일만들기/Solution.java
+++ programmers_lv2/src/april_4_9/타일만들기/Solution.java
@@ -8,7 +8,6 @@ public class Solution {
         int[] dp = new int[n + 1];
         dp[1] = 1;
         dp[2] = 2;
-        answer = dp[1] + dp[2];
         for(int i = 3; i<dp.length;i++){
             dp[i] = (dp[i - 1] + dp[i - 2])%1000000007;
         }
