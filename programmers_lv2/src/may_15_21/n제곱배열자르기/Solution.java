62756da99980bd8a5c93e853a7973d0c43a96fd8
JeonB <newjeonb@gmail.com>
2023-05-24 21:26:37 +0900

    n제곱배열자르기

--- programmers_lv2/src/may_15_21/n제곱배열자르기/Solution.java
+++ programmers_lv2/src/may_15_21/n제곱배열자르기/Solution.java
@@ -1,45 +1,36 @@
 package may_15_21.n제곱배열자르기;
 
+/**
+ * 정수 n, left, right가 주어집니다. 다음 과정을 거쳐서 1차원 배열을 만들고자 합니다.
+ *
+ * n행 n열 크기의 비어있는 2차원 배열을 만듭니다.
+ * i = 1, 2, 3, ..., n에 대해서, 다음 과정을 반복합니다.
+ * 1행 1열부터 i행 i열까지의 영역 내의 모든 빈 칸을 숫자 i로 채웁니다.
+ * 1행, 2행, ..., n행을 잘라내어 모두 이어붙인 새로운 1차원 배열을 만듭니다.
+ * 새로운 1차원 배열을 arr이라 할 때, arr[left], arr[left+1], ..., arr[right]만 남기고 나머지는 지웁니다.
+ * 정수 n, left, right가 매개변수로 주어집니다. 주어진 과정대로 만들어진 1차원 배열을 return 하도록 solution 함수를 완성해주세요.
+ */
 class Solution {
     public int[] solution(int n, long left, long right) {
         int[] answer = new int[(int)(right-left)+1];
-        // n행 n열 2차원배열 생성
-        int[][] double_arr = new int[n][n];
 
-        // i행 i열까지 숫자 i로 채우기
-        for(int i = 1 ; i<=double_arr.length ; i++){
-            for(int j = 0 ; j<i; j++){
-                for(int k = 0; k<= j ; k++){
-                    double_arr[j][k] = j+1;
-                    double_arr[k][j] = j+1;
-                }
+        /* n^n 배열과 그 값을 담을 1차원 배열
+        * arr[0 [0,0]],arr[1 [0,1]],arr[2 [0,2]],..
+        * arr[0 [0/n,0%n]],arr[1 [1/n,1%n]],arr[2 [2/n,2%n]]
+        * */
+        long a = 0;
+        long b = 0;
+        int index = 0 ;
+        for (long i = left; i <= right; i++) {
+             a = i / n;
+             b = i % n;
+            if (a < b) {
+                answer[index++] = (int) b + 1;
+                continue;
             }
+            answer[index++] = (int) a +1;
         }
 
-        int row_l = (int)left/(n-1);
-        int culumn_l = (int)left%n-1;
-        if(culumn_l<0)
-            culumn_l = n-1;
-        int row_r = (int)right/(n-1);
-        int culumn_r = (int)right%n-1;
-        if(culumn_r<0)
-            culumn_r = n-1;
-        // arr[left] ~ arr[right] 만 남긴 값을 answer에 넣음
-        int cnt = 0;
-        if(row_l+1 < row_r){
-            cnt =n;
-        }
-
-        for(int i = row_l+1 ; i<=row_r-1 ; i++ ){
-            for(int j = 0; j<n;j++ )
-                answer[cnt++] = double_arr[i][j];
-        }
-        int first = n-culumn_l;
-        int last = n-culumn_r;
-        for(int i = 0; i< first ;i++)
-            answer[i] = double_arr[row_l][culumn_l++];
-        for(int i = 0; i<last ;i++)
-            answer[cnt++] = double_arr[row_r][culumn_r++];
         return answer;
     }
 }
