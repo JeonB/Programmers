fc1d4153970cbfecf94b03c2f0cf849cafc188ab
JeonB <newjeonb@gmail.com>
2023-09-07 17:30:42 +0900

    스택문제 추가 다리지나는 트럭

---
+++ programmers_lv2/src/september_firstweek/다리지나는트럭/Solution.java
@@ -0,0 +1,15 @@
+package september_firstweek.다리지나는트럭;
+
+
+class Solution {
+    /**
+     * @param bridge_length 다리에 오를 수 있는 최대 트럭 수
+     * @param weight 제한 무게
+     * @param truck_weights 각 트럭 무게
+     * @return 모든 트럭이 다리를 통과하는데 걸리는 최소 시간 초
+     */
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        return answer;
+    }
+}
