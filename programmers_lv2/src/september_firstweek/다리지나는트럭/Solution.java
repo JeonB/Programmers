64af86d341366a2ffe051d1cf7ca7c6f18d02c98
JeonB <newjeonb@gmail.com>
2023-09-11 18:59:16 +0900

    프로그래머스 Stack, Queue 다리지나는 트럭 문제

--- programmers_lv2/src/september_firstweek/다리지나는트럭/Solution.java
+++ programmers_lv2/src/september_firstweek/다리지나는트럭/Solution.java
@@ -1,15 +1,47 @@
 package september_firstweek.다리지나는트럭;
 
+import java.util.LinkedList;
+import java.util.Queue;
 
 class Solution {
     /**
      * @param bridge_length 다리에 오를 수 있는 최대 트럭 수
      * @param weight 제한 무게
      * @param truck_weights 각 트럭 무게
-     * @return 모든 트럭이 다리를 통과하는데 걸리는 최소 시간 초
+     * @return 모든 트럭이 다리를 통과 하는데 걸리는 최소 시간 초
      */
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
+
+        Queue<Integer> trucks = new LinkedList<>();
+
+        int weights = 0;
+        for (int truck_weight : truck_weights) {
+
+            while (true) {
+                if (trucks.isEmpty()) {
+                    trucks.offer(truck_weight);
+                    weights += truck_weight;
+                    answer++;
+                    break;
+                } else if (trucks.size() == bridge_length) {
+                    weights -= trucks.poll();
+                } else {
+                    if (truck_weight + weights <= weight) {
+                        trucks.offer(truck_weight);
+                        weights += truck_weight;
+                        answer++;
+                        break;
+                    } else {
+                        trucks.offer(0);
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        answer += bridge_length;
+
         return answer;
     }
 }
---
+++ programmers_lv2/test/september_firstweek/다리지나는트럭/SolutionTest.java
@@ -0,0 +1,19 @@
+package september_firstweek.다리지나는트럭;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+    @Test
+    public void solution() {
+
+        // given
+        Solution test = new Solution();
+        int[] arr = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10,};
+        // whenㅇㅇㅇㄴ
+        assertEquals(test.solution(100, 100, arr), 110);// 자동 완성 후 커서 위치
+
+        // then
+    }
+}
