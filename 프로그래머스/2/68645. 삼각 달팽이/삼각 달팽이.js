03d3d50e464f366d7f536c177d6f2bacaa6da9c5
JeonB <newjeonb@gmail.com>
2024-06-15 18:16:37 +0900

    [level 2] Title: 삼각 달팽이, Time: 131.90 ms, Memory: 98.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
@@ -0,0 +1,28 @@
+function solution(n) {
+    // 삼각형을 저장할 2차원 배열을 초기화합니다.
+    const triangle = Array.from({ length: n }, (_, i) => Array(i + 1).fill(0));
+    let num = 1; // 삼각형에 채울 숫자
+    let x = -1, y = 0; // 시작 위치
+
+    for (let i = 0; i < n; i++) {
+        for (let j = i; j < n; j++) {
+            if (i % 3 === 0) {
+                // 아래로 이동
+                x++;
+            } else if (i % 3 === 1) {
+                // 오른쪽으로 이동
+                y++;
+            } else if (i % 3 === 2) {
+                // 위로 이동
+                x--;
+                y--;
+            }
+            triangle[x][y] = num++;
+        }
+    }
+
+    // 2차원 배열을 1차원 배열로 변환하여 반환합니다.
+    return triangle.flat();
+}
+
+
