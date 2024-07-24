775712738f3d30bee182b1c1ebe2625302cbe972
JeonB <newjeonb@gmail.com>
2024-07-24 09:09:12 +0900

    [Bronze V] Title: 시험 성적, Time: 128 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,36 @@
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int point = Integer.parseInt(br.readLine());
+
+        switch (point/10){
+            case 10:
+            case 9:bw.write("A");
+                    break;
+            case 8: bw.write("B");
+                    break;
+            case  7: bw.write("C");
+                    break;
+            case 6 : bw.write("D");
+                    break;
+            default : bw.write("F");
+        }
+
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    }
+
