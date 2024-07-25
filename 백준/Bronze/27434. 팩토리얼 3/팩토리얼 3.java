3c58865d94cc07671017837c6ea500b45f6fa6f9
JeonB <newjeonb@gmail.com>
2024-07-25 17:42:33 +0900

    [Bronze V] Title: 팩토리얼 3, Time: 3216 ms, Memory: 389796 KB -BaekjoonHub

---
+++ 백준/Bronze/27434. 팩토리얼 3/팩토리얼 3.java
@@ -0,0 +1,28 @@
+import java.math.BigInteger;
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        int N = Integer.parseInt(br.readLine());
+        BigInteger answer = N == 0 ? BigInteger.valueOf(1) : facto(1,N);
+        bw.write(answer+ "\n");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static BigInteger facto(int a , int b){
+        if(a == b) return BigInteger.valueOf(a);
+        int mid = (a+b)/2;
+        return facto(a,mid).multiply(facto(mid+1,b));
+    }
+
+}
+
