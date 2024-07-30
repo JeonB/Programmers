775ddcf4093547b840be99a8b10b69b716009d4a
JeonB <newjeonb@gmail.com>
2024-07-30 10:03:21 +0900

    [Bronze I] Title: 컵홀더, Time: 144 ms, Memory: 15996 KB -BaekjoonHub

---
+++ 백준/Bronze/2810. 컵홀더/컵홀더.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        int people = Integer.parseInt(br.readLine());
+        String seats = br.readLine();
+        if (seats.length() < people) {
+            System.out.println("인구수 맞추셈");
+            return;
+        }
+
+        seats = seats.replaceAll("LL", "L");
+        if(seats.length() + 1  > people) bw.write(people+ "\n");
+        else bw.write((seats.length() + 1 )+ "\n");
+
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+
+}
+
