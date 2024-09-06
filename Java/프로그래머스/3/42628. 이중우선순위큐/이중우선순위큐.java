6386f06798a5a991e728a669ee135de5bd1cb20c
JeonB <newjeonb@gmail.com>
2024-09-06 10:14:06 +0900

    [level 3] Title: 이중우선순위큐, Time: 77.80 ms, Memory: 127 MB -BaekjoonHub

---
+++ Java/프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] operations) {
+        int[] answer;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        
+        for(String operation : operations){
+            String[] op = operation.split(" ");
+
+            if(op[0].equals("I")){
+                int num = Integer.valueOf(op[1]);
+                minHeap.offer(num);
+                maxHeap.offer(num);
+            }else{
+                if(minHeap.isEmpty()) continue;
+                int maxValue = maxHeap.peek();
+                int minValue = minHeap.peek();
+                if(op[1].equals("1")){
+                    maxHeap.poll();
+                    minHeap.remove(maxValue);
+                }else{
+                    minHeap.poll();
+                    maxHeap.remove(minValue);
+                }
+            }
+        }
+        
+        if(minHeap.isEmpty() && maxHeap.isEmpty()) return new int[]{0,0};
+        
+        answer = new int[]{maxHeap.peek(),minHeap.peek()};
+        return answer;
+    }
+}
+
+
