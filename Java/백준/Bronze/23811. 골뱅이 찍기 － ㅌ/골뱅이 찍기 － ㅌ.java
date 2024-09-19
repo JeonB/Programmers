ba62833ef6dda3c8bd43c304b7708658c321affe
JeonB <newjeonb@gmail.com>
2024-09-19 18:02:53 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅌ, Time: 136 ms, Memory: 15320 KB -BaekjoonHub

---
+++ Java/백준/Bronze/23811. 골뱅이 찍기 － ㅌ/골뱅이 찍기 － ㅌ.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+public class Main {
+    static boolean[] visited;
+    static ArrayList<ArrayList<Integer>> list;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());  // 노드 개수
+        for(int i = 0; i<N ; i++){
+            for(int j = 0; j<N ; j++){
+                bw.write("@@@@@");
+            }
+            bw.write("\n");
+        }
+        for(int i = 0; i<N ; i++){
+            for(int j = 0; j<N ; j++){
+                bw.write("@");
+            }
+            bw.write("\n");
+        }
+        for(int i = 0; i<N ; i++){
+            for(int j = 0; j<N ; j++){
+                bw.write("@@@@@");
+            }
+            bw.write("\n");
+        }
+        for(int i = 0; i<N ; i++){
+            for(int j = 0; j<N ; j++){
+                bw.write("@");
+            }
+            bw.write("\n");
+        }
+        for(int i = 0; i<N ; i++){
+            for(int j = 0; j<N ; j++){
+                bw.write("@@@@@");
+            }
+            bw.write("\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
