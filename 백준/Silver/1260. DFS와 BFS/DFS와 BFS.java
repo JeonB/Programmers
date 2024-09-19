c24936e6ea5eab151f49adaa602c1baca39da1a2
JeonB <newjeonb@gmail.com>
2024-09-19 20:10:58 +0900

    [Silver II] Title: DFS와 BFS, Time: 284 ms, Memory: 21256 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -0,0 +1,79 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+public class Main {
+    static boolean[] visited;
+    static ArrayList<ArrayList<Integer>> list;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());  // 노드 개수
+        int M = Integer.parseInt(st.nextToken());  // 간선 개수
+        int V = Integer.parseInt(st.nextToken());  // 시작 노드
+        visited = new boolean[N+1];
+        list = new ArrayList<>();
+
+        for(int i =0 ; i<=N ; i++){
+            list.add(new ArrayList<>());
+        }
+        for(int i = 0 ; i<M ; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            list.get(a).add(b);
+            list.get(b).add(a);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(list.get(i));
+        }
+
+        dfs(V);
+        System.out.println();
+        visited = new boolean[N+1];
+        bfs(V);
+        System.out.println();
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static void dfs(int start){
+        if(visited[start]){
+            return;
+        }
+        visited[start] = true;
+        System.out.print(start + " ");
+        for(int i : list.get(start)){
+            if(!visited[i]){
+                dfs(i);
+            }
+        }
+    }
+
+    static void bfs(int start){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        visited[start] = true;
+
+        while(!q.isEmpty()){
+            int node = q.poll();
+            System.out.print(node + " ");
+            for(int i : list.get(node)){
+                if(!visited[i]){
+                    q.add(i);
+                    visited[i] = true;
+                }
+            }
+        }
+    }
+}
