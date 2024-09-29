12446ff1ef1deac23e01856e75943a6798e4a9af
JeonB <newjeonb@gmail.com>
2024-09-29 23:11:16 +0900

    [Bronze II] Title: 숫자 카드놀이, Time: 96 ms, Memory: 14084 KB -BaekjoonHub

---
+++ 백준/Bronze/4564. 숫자 카드놀이/숫자 카드놀이.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String S = br.readLine();
+        while(Integer.parseInt(S) != 0){
+            bw.write(S + " ");
+            while(S.length() > 1){
+                int result = 1;
+                for(int i = 0 ; i < S.length() ; i++){
+                    result *= S.charAt(i) - '0';
+                }
+                S = Integer.toString(result); 
+                bw.write(S + " ");
+            }
+            bw.write("\n");
+            S = br.readLine();
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
