41afb0dfe9bcaa1ec80e3469d82e59544e68d4a7
JeonB <newjeonb@gmail.com>
2024-12-02 11:01:52 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.03 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/README.md
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 자연수 뒤집어 배열로 만들기 - 12932 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12932?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12932?language=java) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.08 ms
+메모리: 84.4 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 29일 15:12:45
+2024년 12월 02일 11:01:49
 
 ### 문제 설명
 
--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -1,11 +1,11 @@
 import java.util.ArrayList;
 class Solution {
     public int[] solution(long n) {
-        
-    String s = String.valueOf(n);
-    int[] answer = new int[s.length()];
-    for(int i = 0 ; i<s.length();i++){
-      answer[i] = s.charAt(s.length() - 1 - i ) - 48;
+    
+    String num = String.valueOf(n);
+    int[] answer = new int[num.length()];
+    for(int i = num.length()-1 ; i>-1 ; i--){
+        answer[num.length()-1-i] =  num.charAt(i) - '0';
     }
     return answer;
     }
