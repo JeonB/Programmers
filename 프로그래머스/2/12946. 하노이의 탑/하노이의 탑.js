c4d30d36e65dfd1e2efaa46bdd7964cc10eb3227
JeonB <newjeonb@gmail.com>
2024-12-22 14:10:48 +0900

    [level 2] Title: 하노이의 탑, Time: 4.87 ms, Memory: 48.6 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.js
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.js
@@ -1,15 +1,13 @@
 function solution(n) {
-    var answer = [];
-
-    function hanoi(n, from, to) {
+    let answer = [];
+    
+    function hanoi(n, from , to){
         if(n === 1) return answer.push([from,to])
         
-        hanoi(n-1, from,6-(from+to))
-        answer.push([from,to])
-        hanoi(n-1, 6-(from+to),to)
+        const empty = 6 - from - to 
+        return hanoi(n-1,from, empty) +hanoi(1,from, to)+hanoi(n-1,empty,to)
     }
-
-    hanoi(n, 1, 3);
-
+    
+    hanoi(n,1,3)
     return answer;
 }
