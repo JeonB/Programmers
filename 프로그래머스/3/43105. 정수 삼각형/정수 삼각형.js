53cea9692b894d42c9f780a225be1a6065ef08d7
JeonB <newjeonb@gmail.com>
2025-01-23 21:22:46 +0900

    [level 3] Title: 정수 삼각형, Time: 3.15 ms, Memory: 40.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -0,0 +1,17 @@
+function solution(triangle) {
+        
+    const n = triangle.length;
+
+    const dp = [...triangle[n - 1]];
+
+
+    for (let i = n - 2; i >= 0; i--) {
+        for (let j = 0; j <= i; j++) {
+
+            dp[j] = triangle[i][j] + Math.max(dp[j], dp[j + 1]);
+        }
+    }
+
+
+    return dp[0];
+}
