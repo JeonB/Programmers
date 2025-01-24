38e8ab1cfd9cfa7e8e01858738a87d60330fc8fd
JeonB <newjeonb@gmail.com>
2025-01-24 15:16:09 +0900

    [level 3] Title: 정수 삼각형, Time: 9.23 ms, Memory: 43.4 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/README.md
+++ 프로그래머스/3/43105. 정수 삼각형/README.md
@@ -1,10 +1,10 @@
 # [level 3] 정수 삼각형 - 43105 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105?language=javascript) 
 
 ### 성능 요약
 
-메모리: 40.4 MB, 시간: 3.15 ms
+메모리: 43.4 MB, 시간: 9.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 23일 21:22:45
+2025년 01월 24일 15:16:06
 
 ### 문제 설명
 
--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,17 +1,12 @@
 function solution(triangle) {
-        
-    const n = triangle.length;
-
-    const dp = [...triangle[n - 1]];
-
-
-    for (let i = n - 2; i >= 0; i--) {
-        for (let j = 0; j <= i; j++) {
-
-            dp[j] = triangle[i][j] + Math.max(dp[j], dp[j + 1]);
-        }
-    }
-
-
-    return dp[0];
+  const n = triangle.length
+  const dp = Array.from({length:n}, () => Array(n).fill(-1))
+  
+  function memo(i,j,triangle) {
+      if(i === n) return 0
+      if(dp[i][j] !== -1) return dp[i][j]
+      
+      return dp[i][j] = triangle[i][j] +Math.max(memo(i+1,j,triangle),memo(i+1,j+1,triangle))
+  }
+    return memo(0,0,triangle)
 }
