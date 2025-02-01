7d3119441daacdac9b7caa930008d153cdbdcf27
JeonB <newjeonb@gmail.com>
2025-02-01 18:03:08 +0900

    [level 3] Title: 이중우선순위큐, Time: 452.28 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.js
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.js
@@ -1,20 +1,58 @@
-function solution(operations) {
-    let queue = [];
+class DualPriorityQueue {
+    constructor() {
+        this.minHeap = [];
+        this.maxHeap = new Map(); // 삭제된 요소를 추적하기 위한 맵
+    }
 
-    operations.forEach(op => {
+    insert(value) {
+        this.minHeap.push(value);
+        this.minHeap.sort((a, b) => a - b); // 최소 힙 유지
+        this.maxHeap.set(value, (this.maxHeap.get(value) || 0) + 1); // 개수 저장
+    }
+
+    deleteMax() {
+        while (this.minHeap.length) {
+            const maxVal = this.minHeap.pop();
+            if (this.maxHeap.has(maxVal) && this.maxHeap.get(maxVal) > 0) {
+                this.maxHeap.set(maxVal, this.maxHeap.get(maxVal) - 1);
+                if (this.maxHeap.get(maxVal) === 0) this.maxHeap.delete(maxVal);
+                return;
+            }
+        }
+    }
+
+    deleteMin() {
+        while (this.minHeap.length) {
+            const minVal = this.minHeap.shift();
+            if (this.maxHeap.has(minVal) && this.maxHeap.get(minVal) > 0) {
+                this.maxHeap.set(minVal, this.maxHeap.get(minVal) - 1);
+                if (this.maxHeap.get(minVal) === 0) this.maxHeap.delete(minVal);
+                return;
+            }
+        }
+    }
+
+    getMinMax() {
+        const validNumbers = this.minHeap.filter(val => this.maxHeap.has(val) && this.maxHeap.get(val) > 0);
+        return validNumbers.length ? [validNumbers[validNumbers.length - 1], validNumbers[0]] : [0, 0];
+    }
+}
+
+function solution(operations) {
+    const dpq = new DualPriorityQueue();
+
+    for (const op of operations) {
         const [command, num] = op.split(" ");
         const value = Number(num);
 
         if (command === "I") {
-            queue.push(value);
-        } else if (command === "D" && queue.length) {
-            queue.sort((a, b) => a - b);
-            value === 1 ? queue.pop() : queue.shift();
+            dpq.insert(value);
+        } else if (command === "D" && value === 1) {
+            dpq.deleteMax();
+        } else if (command === "D" && value === -1) {
+            dpq.deleteMin();
         }
-    });
+    }
 
-    if (queue.length === 0) return [0, 0];
-
-    queue.sort((a, b) => a - b);
-    return [queue[queue.length - 1], queue[0]];
+    return dpq.getMinMax();
 }
