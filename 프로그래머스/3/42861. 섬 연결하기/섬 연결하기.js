5075b69cb6b8b3d3273bbf60f3f5836fbd4de819
JeonB <newjeonb@gmail.com>
2025-02-10 20:54:16 +0900

    [level 3] Title: 섬 연결하기, Time: 0.32 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
@@ -1,35 +1,29 @@
 function solution(n, costs) {
-    // 비용을 기준으로 오름차순 정렬
-    costs.sort((a, b) => a[2] - b[2]);
+    costs.sort((a,b) => a[2] - b[2])
+    const parent = Array.from({length:n}, (_,i) => i)
     
-    // Union-Find 자료구조를 위한 배열
-    const parent = Array.from({ length: n }, (_, i) => i);
-    
-    function find(x) {
-        if (parent[x] === x) return x;
-        return parent[x] = find(parent[x]);
+    function find(x){
+        if(x === parent[x]) return x
+        return parent[x] = find(parent[x])
     }
-    
-    function union(x, y) {
-        const rootX = find(x);
-        const rootY = find(y);
-        if (rootX !== rootY) {
-            parent[rootY] = rootX;
-            return true;
+    function union(a,b){
+        const rootA = find(a)
+        const rootB = find(b)
+        if(rootA !== rootB){
+            parent[rootB] = rootA
+            return true
         }
-        return false;
+        
+        return false
     }
-    
-    let totalCost = 0;
-    let edgesUsed = 0;
-    
-    for (const [a, b, cost] of costs) {
-        if (union(a, b)) { // 사이클이 생기지 않으면 연결
-            totalCost += cost;
-            edgesUsed++;
-            if (edgesUsed === n - 1) break; // 최소 신장 트리가 완성되면 종료
+    let totalCost = 0
+    let minimalEdge = 0
+    for(const [a,b,cost] of costs){
+        if(union(a,b)){
+            totalCost += cost
+            minimalEdge++
+            if(minimalEdge === n-1) break
         }
     }
-    
-    return totalCost;
+    return totalCost
 }
